bb0d274e20dc78608406b1459439dc213f4d4f54
Flavioeds82 <flavio_eduardo@id.uff.br>
2022-05-14 20:53:19 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Initial Data
 let currentQuestion = 0;
 let correctAnswers = 0;
+let labelAnswers = document.querySelector('.label');
+let unlock = true;
 showQuestion();
 
 // Events
@@ -8,12 +10,14 @@ document.querySelector('.scoreArea button').addEventListener('click', resetEvent
 
 // Functions
 function showQuestion() {
+    
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
         let pct = Math.floor((currentQuestion / questions.length) * 100);
         document.querySelector('.progress--bar').style.width = `${pct}%`;
 
+        
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
 
@@ -33,15 +37,49 @@ function showQuestion() {
 }
 
 function optionClickEvent(e) {
-    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+   if(unlock){
+      let clickedOption = parseInt(e.target.getAttribute('data-op'));
+    let correctElem = document.querySelector(`div[data-op="${questions[currentQuestion].answer}"]`);
+    let hit = false;
+    unlock = false;
 
-    if(questions[currentQuestion].answer === clickedOption) {
-        correctAnswers++;
+      // ACERTO DA QUESTÃO
+
+    if(questions[currentQuestion].answer === clickedOption) { 
+      correctAnswers++;
+      hit = true;
+      correctElem.classList.add('.infoProgressAnimation');
+      correctElem.style.backgroundColor = 'green';
+      correctElem.style.color = "#fff";
+      labelAnswers.style.display = 'block';
+      labelAnswers.innerHTML = 'Resposta correta!'
+      labelAnswers.style.backgroundColor = 'green';
+      labelAnswers.style.color = '#fff';
+        
+    }else{
+      labelAnswers.style.display = 'block';
+      correctElem.style.backgroundColor = 'green';
+      correctElem.style.color = "#fff";
+      e.target.style.backgroundColor = 'red';
+      e.target.style.color = "#fff";
+      labelAnswers.innerHTML = 'Resposta errada!';
+      labelAnswers.style.backgroundColor = 'red';
+      labelAnswers.style.color = '#fff';
+      
     }
-
-    currentQuestion++;
-    showQuestion();
+    correctElem.classList.add('infoProgressAnimation');
+    correctElem.style.backgroundColor = 'green';
+    correctElem.style.color = "#fff";
+    
+    setTimeout(() => {
+      currentQuestion++;
+      labelAnswers.style.display = 'none';
+      showQuestion();
+      unlock = true;
+  }, 2000);
 }
+   }
+    
 
 function finishQuiz() {
     let points = Math.floor((correctAnswers / questions.length) * 100);
